c28f25e30773541b342629c55fab1a6b5bca1aab
ethanbrian <mugoethan@gmail.com>
2024-06-20 10:11:33 +0300

    new file

--- translation/settings.py
+++ translation/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-ew-v0&$_l$ppaiyo0iq$mluqb080i71iqc!z1fob3pjo=@z5sg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']  # Add localhost and appropriate IP addresses here
-
+# Add 'djangolugha.drlugha.com' to ALLOWED_HOSTS
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'djangolugha.drlugha.com']
 
 # Application definition
 
@@ -45,7 +45,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Make sure it's one of the first middleware classes
@@ -78,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'translation.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -88,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -108,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -119,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
